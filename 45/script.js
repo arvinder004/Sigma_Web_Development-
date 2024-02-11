af4d0d9051f584545c1df1eb6ebe9b8c75ba3482
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2024-02-11 22:39:46 +0530

    javascript conditionals

--- 45/script.js
+++ 45/script.js
@@ -1,1 +1,41 @@
 console.log("conditional tutorial")
+
+/* if-else statement syntax:
+    if(condition)
+    { 
+        statement
+    } 
+    else
+    {
+        statement
+    }*/
+
+let age = 45;
+
+if(age>=18){
+    console.log("you can vote")
+}
+else{
+    console.log("you cant vote")
+}
+
+/* operators in JavaScript -: +     -     *    **(exponentiation)    /      %       ++      -- */
+
+let grace = 2;
+
+console.log(age ** grace)
+
+/*  === (equal value and equal type) -> checks type and value  */
+
+3 == '3'  //true (checks only value)
+3 === '3' //false (checks type and value)
+
+/* !== (not equal value or not equal type) */
+
+/* && logical and    || logical or   ! logical not */
+
+
+//ternary operator ---->  condition ? if condition is true this statement will execute : else thi one
+
+
+let vote = age>=18 ? console.log("can vote") : console.log("cannot vote")
