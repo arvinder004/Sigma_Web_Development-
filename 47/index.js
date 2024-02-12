2a7d14bc55ff59677bf0e3f7e7490b72af4c745d
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2024-02-13 00:24:52 +0530

    functions in js

--- 47/index.js
+++ 47/index.js
@@ -6,3 +6,27 @@ function greeting(name) {
 
 greeting("arvinder")
 greeting("harshit")
+
+function sum1(a , b){
+    console.log (a+b)
+}
+
+sum1(3,5)
+
+function sum2(a , b){
+    return a+b
+}
+
+result = sum2(10,29)
+
+console.log(result)
+
+
+//arrow function
+
+const func1 = (x,y)=>{
+    console.log("i am an arrow function", x)
+    console.log("i am an arrow function", y)
+}
+
+func1(20,32);
