ee24ee81a14ae6446af3ee0b980627108a7d5e98
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2024-04-14 14:09:02 +0530

    JS strings

--- 49/index.js
+++ 49/index.js
@@ -1,2 +1,36 @@
 console.log("JavaScript Strings")
 
+let a = "Arvinder";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+
+console.log(a.length)
+
+let name = "arvinder"
+let friend = "vidushi"
+
+console.log("my name is "+name+" and my friends name is "+friend)
+
+//template literals
+console.log(`my name is ${name} and my friends name is ${friend}`)
+
+friend = "vidushi"
+console.log(friend.toUpperCase())
+
+console.log(friend.toLowerCase())
+
+console.log(friend.length)
+
+console.log(friend.slice(1,3))
+console.log(friend.slice(4))
+console.log(friend.slice(1))
+
+console.log(friend.replace("vi","69"))
+
+
