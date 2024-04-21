d66444d6eb7e0836d3bc9174e9e4d984a7355c5f
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2024-04-21 15:35:30 +0530

    js promises

---
+++ 60/promise.js
@@ -0,0 +1,35 @@
+console.log('this is promises')
+
+// let prom1 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("yes i am done")
+//         resolve("arvinder")
+//     }, 3000);
+// })
+
+// prom1.then((a=>{
+//     console.log(a)
+// }))
+
+//promise to generate random number greater than 0.5
+//if number is less than 0.5 then it will reject the number
+
+let prom1 = new Promise((resolve, reject) => {
+  let a = Math.random()
+  if(a <= 0.5){
+    reject("random number not supported ")
+  }
+
+  else{
+    console.log("random number generated ")
+    resolve(a)
+  }
+})
+
+prom1.then((a) => {
+    console.log(a)
+}).catch((err)=>{
+    console.log(err)
+})
+
+
