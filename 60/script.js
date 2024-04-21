d214ea58277753dfff219488907ea9b09e93005f
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2024-04-21 15:35:03 +0530

    js callback functions

---
+++ 60/script.js
@@ -0,0 +1,24 @@
+//CALL BACK FUCNTIONS
+//CALLBACK is a function passed as an argument to another function. This technique allows a function to call another function. A callback function can run after another function has finished
+
+//javascript functions are executed in the sequence they are called, not in the sequence they are defined
+
+// function first(){
+//     console.log("hello")
+// }
+
+// function second(){
+//     console.log("world")
+// }
+
+// first();
+// second();
+// output hello world
+
+// second();
+// first();
+//output world hello
+
+function display(s){
+    document.getElementById
+}
